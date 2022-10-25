c3d176f9ef3a2dcbc66c7cf7bcc4350ca4cee397
Angelo Mantellini <manangel@cisco.com>
2022-10-26 01:04:40 +0200

    feat: publish android aar to packages
    Ref: #30 #promote PATCH
    Signed-off-by: Angelo Mantellini <manangel@cisco.com>

--- android/scripts/publish_common_aar.sh
+++ android/scripts/publish_common_aar.sh
@@ -4,32 +4,33 @@ VERSION_CODE=1
 GITHUB_USER=""
 GITHUB_TOKEN=""
 MVN_REPO=""
-while getopts ":v:u:t:r:" opt; do
-  case $opt in
-    v) VERSION_CODE="$OPTARG"
-    ;; 
-    u) GITHUB_USER="$OPTARG"
-    ;;
-    t) GITHUB_TOKEN="$OPTARG"
-    ;;
-    r) MVN_REPO="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    ;;
-  esac
-done
+# while getopts ":v:u:t:r:" opt; do
+#   case $opt in
+#     v) VERSION_CODE="$OPTARG"
+#     ;; 
+#     u) GITHUB_USER="$OPTARG"
+#     ;;
+#     t) GITHUB_TOKEN="$OPTARG"
+#     ;;
+#     r) MVN_REPO="$OPTARG"
+#     ;;
+#     \?) echo "Invalid option -$OPTARG" >&2
+#     ;;
+#   esac
+# done
 
 
-cd aar_modules/CommonLibrary
+cd CommonLibrary
 if [ ! -f local.properties ]; then
 	echo sdk.dir=${SDK} > local.properties
 fi
 
 
-if [ "$MVN_REPO" = "" ]; then
-    ./gradlew publish -PVERSION=$VERSION_CODE -PGITHUB_USER=$GITHUB_USER -PGITHUB_TOKEN=$GITHUB_TOKEN
-else
-    ./gradlew publish -PVERSION=$VERSION_CODE -PGITHUB_USER=$GITHUB_USER -PGITHUB_TOKEN=$GITHUB_TOKEN -PMVN_REPO=$MVN_REPO
+#if [ "$MVN_REPO" = "" ]; then
+#    ./gradlew publish -PVERSION=$VERSION_CODE -PGITHUB_USER=$GITHUB_USER -PGITHUB_TOKEN=$GITHUB_TOKEN
+#else
+    gradle publish -PVERSION=$VERSION_CODE 
+    #-PGITHUB_USER=$GITHUB_USER -PGITHUB_TOKEN=$GITHUB_TOKEN -PMVN_REPO=$MVN_REPO
 fi
 
 echo "aar published"
